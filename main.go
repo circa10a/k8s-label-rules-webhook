1bc1b19702ec4de95b779691bd0905304917173a
Caleb Lemoine <caleblemoine@gmail.com>
2019-12-14 00:28:42 -0600

    add flags

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
+func flags() {
+	filePath := flag.String("file", "", "Path to yaml file with ruleset")
+	flag.Parse()
+	if *filePath == "" {
+		log.Fatal("No file provided")
+	}
+}
+
 func main() {
+	// Validate command line arguments
+	flags()
 	// Test rules
-	// rules := getRules("./rules.yaml")
-	// log.Info(rules[0])
+	rules := getRules(*filePath)
+	log.Info(rules[0])
 	// match, _ := regexp.MatchString(rules[0].Value.Regex, "318-305-6964")
 	// fmt.Println(match)
 	// Initialize gin engine
