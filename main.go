c50bb9551c93c217725f2e16fbd283a2a83c72c5
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2021-02-18 19:54:12 -0600

    #patch: bump deps, go 1.16 (#39)

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func flags() {
 }
 
 // @title k8s-label-rules-webhook
-// @version 0.2.9
+// @version 0.2.10
 // @description A kubernetes webhook to standardize labels on resources
 
 // @contact.name GitHub
